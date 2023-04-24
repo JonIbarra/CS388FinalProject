7d9d699226eec2ddbd44d200d470e4ab52dba8ff
AsierAzpiri <asier.azpiri@digipen.edu>
2023-04-24 18:22:56 +0200

    Working on menus

---
+++ CS388FinalProject/Assets/LevelEnum.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Level { None, Gold, Sword, Cup, Cane }
+
+public class LevelEnum : MonoBehaviour
+{
+
+    public Level whichLevel = Level.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
